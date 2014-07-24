f6055a6893e1838a6f1beb7bd192bdd48ea53fd0
Brad Perkins <bperkins99@fullsail.edu>
2014-07-24 11:15:08 -0400

    First commit for this project
    added global var, and procedure, and started boolean which nested loop.

--- project3.js
+++ project3.js
@@ -1,1 +1,30 @@
-alert("JavaScript works!");
+// Bradley Perkins
+// SDI 1407
+// Project 2
+// 7-15-14
+
+
+// Global Variables
+
+var bike = "mountain bike ";
+var rack = "bike rack";
+var air = true;
+var ready = "It looks like you\'re ready to go!";
+var maxPsi =  65 ;
+
+// Procedure
+var totalBikes = function(numBikes){
+	if (numBikes <= 4){
+		console.log("The " + numBikes + " bikes will fit on the rack together.");
+	}else {
+		console.log("All of the bikes will not fit on the rack at the same time.");
+	}
+};
+totalBikes(3);
+
+
+
+//Boolean function with nested loop.
+var rainGear = function(rainCoat){
+	var 
+}
